893fc6725680ce281dc7737bcf21226c7a8f7420
nathanpriyasadie <nathanpriyasadie@gmail.com>
2020-10-20 19:34:29 +0700

    redirect endpoint

--- src/components/LinkCard.js
+++ src/components/LinkCard.js
@@ -4,7 +4,7 @@ export default function LinkCard({ link, refreshLinks }) {
     const archiveLink = async () => {
         link.archived = !link.archived;
         try {
-            await fetch('/api/updateLink', {
+            await fetch('/.netlify/functions/updateLink', {
                 method: 'PUT',
                 body: JSON.stringify(link)
             })
@@ -16,7 +16,7 @@ export default function LinkCard({ link, refreshLinks }) {
     const deleteLink = async () => {
         console.log(link._id)
         try {
-            await fetch('/api/deleteLink', {
+            await fetch('/.netlify/functions/deleteLink', {
                 method: 'DELETE',
                 body: JSON.stringify({ _id: link._id })
             })
